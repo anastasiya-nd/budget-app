f74a5b324ba1a321dc105a2da7bc140688346a0d
Anastasiya <nast_danko_95@mail.ru>
2021-07-29 15:49:00 +0300

    feature: Create connection to mongoDB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -8,4 +9,15 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+mongoose.connect('mongodb://localhost:27017/some-mongo', {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connection.on('error', (err) => {
+  console.error("Database Connection Error: " + err)
+  process.exit(1)
+})
+
+mongoose.connection.once('connected', () => {
+  console.info("Succesfully connected to MongoDB Database")
 })
