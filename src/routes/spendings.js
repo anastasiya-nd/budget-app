ee825eac2b87e2995ca2df7f26f1d17d621506d0
Anastasiya <nast_danko_95@mail.ru>
2021-10-25 19:18:00 +0300

    chore: Change endpoint

--- src/routes/spendings.js
+++ src/routes/spendings.js
@@ -3,11 +3,11 @@ const controller = require('../controllers/spendings')
 
 const router = express()
 
-router.post('/spendings/', controller.createSpending)
-router.get('/spendings/', controller.getSpendings)
+router.post('/spendings', controller.createSpending)
+router.get('/spendings', controller.getSpendings)
 router.delete('/spendings/:id', controller.deleteSpending)
 router.get('/spendings/:id', controller.getSpending)
 router.patch('/spendings/:id', controller.updateSpending)
-router.get('/chart/', controller.getChartData)
+router.get('/chart', controller.getChartData)
 
 module.exports = router
